b5409a91c7f4839c804e66878ba3b9146cc2c67f
F4R3Juan58 <juan58coc@gmail.com>
2025-03-18 17:55:25 +0100

    RegistroMerge branch 'main' of https://github.com/F4R3Juan58/TFGServerClientLimpieza de archivos temporales y configuración local del repositorio

---
+++ TFGClient/Registro.xaml.cs
@@ -0,0 +1,63 @@
+﻿using System.Net.Mail;
+using System.Net;
+
+namespace TFGClient
+{
+    public partial class Registro : ContentPage
+    {
+        private string codigoGenerado = string.Empty;
+
+        public Registro()
+        {
+            InitializeComponent();
+        }
+
+        private async void OnVerificacionClicked(object sender, EventArgs e)
+        {
+            var random = new Random();
+            codigoGenerado = random.Next(100000, 999999).ToString();
+            try
+            {
+                MailMessage mail = new MailMessage();
+                mail.From = new MailAddress("tfg.educamadrid.org@gmail.com");
+                mail.To.Add(EmailEntry.Text);
+                mail.Subject = "Tu código de verificación";
+                mail.Body = $"Tu código es: {codigoGenerado}";
+
+                SmtpClient smtp = new SmtpClient("smtp.gmail.com", 587);
+                smtp.Credentials = new NetworkCredential("tfg.educamadrid.org@gmail.com", "mvmxasnwxgqgidxf");
+                smtp.EnableSsl = true;
+
+                smtp.Send(mail);
+
+                await DisplayAlert("Éxito", "Correo enviado correctamente", "OK");
+            }
+            catch (Exception ex)
+            {
+                await DisplayAlert("Error", $"No se pudo enviar el correo: {ex.Message}", "OK");
+            }
+            Formulario.IsVisible = false;
+            Verificacion.IsVisible = true;
+        }
+
+        private void OnContinuarClicked(object sender, EventArgs e)
+        {
+            if (CodigoEntry.Text == codigoGenerado)
+            {
+                Formulario2.IsVisible = true;
+                Verificacion.IsVisible = false;
+            }
+            else
+            {
+                DisplayAlert("Error","El codigo que has introducido no es correcto","Ok");
+            }
+            
+        }
+
+        private void OnRegisterClicked(object sender, EventArgs e)
+        {
+            DisplayAlert("Registro", "Registro completado", "OK");
+        }
+    }
+
+}
