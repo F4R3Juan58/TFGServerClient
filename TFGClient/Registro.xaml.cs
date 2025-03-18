46ff980a9bb48b61ba3e9371604f3ab182fbb54c
F4R3Juan58 <juan58coc@gmail.com>
2025-03-18 17:55:25 +0100

    Limpieza de archivos temporales y configuración local del repositorio

---
+++ TFGClient/Registro.xaml.cs
@@ -0,0 +1,28 @@
+﻿namespace TFGClient
+{
+    public partial class Registro : ContentPage
+    {
+        public Registro()
+        {
+            InitializeComponent();
+        }
+
+        private void OnVerificacionClicked(object sender, EventArgs e)
+        {
+            Formulario.IsVisible = false;
+            Verificacion.IsVisible = true;
+        }
+
+        private void OnContinuarClicked(object sender, EventArgs e)
+        {
+            Formulario2.IsVisible = true;
+            Verificacion.IsVisible = false;
+        }
+
+        private void OnRegisterClicked(object sender, EventArgs e)
+        {
+            DisplayAlert("Registro", "Registro completado", "OK");
+        }
+    }
+
+}
