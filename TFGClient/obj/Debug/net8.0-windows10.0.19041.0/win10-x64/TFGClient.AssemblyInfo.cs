cd183190c7c6ae383280c505474f442ae6170ad4
F4R3Juan58 <juan58coc@gmail.com>
2025-03-20 14:15:24 +0100

    Login 1

--- TFGClient/obj/Debug/net8.0-windows10.0.19041.0/win10-x64/TFGClient.AssemblyInfo.cs
+++ TFGClient/obj/Debug/net8.0-windows10.0.19041.0/win10-x64/TFGClient.AssemblyInfo.cs
@@ -18,7 +18,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("TFGClient")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0+c724c00271183056596c5d4ca8b483d97b2c831e")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0+80c8251558d00e22ab08a74587a85911cda99aa2")]
 [assembly: System.Reflection.AssemblyProductAttribute("TFGClient")]
 [assembly: System.Reflection.AssemblyTitleAttribute("TFGClient")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
