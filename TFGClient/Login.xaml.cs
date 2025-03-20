7f7f24a9a0a9185662639f8f28866280c58d31ae
F4R3Juan58 <juan58coc@gmail.com>
2025-03-20 14:15:24 +0100

    Login 1

--- TFGClient/Login.xaml.cs
+++ TFGClient/Login.xaml.cs
@@ -9,7 +9,17 @@ public partial class Login : ContentPage
 
     private async void onLoginClicked(object sender, EventArgs e)
     {
-        await Navigation.PushAsync(new ProfesorHome());
+        
+        bool isProferor = false;
+        if (isProferor == false)
+        {
+            var uri = new Uri("https://discord.gg/f3YA784A");
+            await Launcher.Default.OpenAsync(uri);
+        }
+        else
+        {
+            await Navigation.PushAsync(new ProfesorHome());
+        }
     }
 
     private async void onRegistroClicked(object sender, EventArgs e)
