eb4c87e0c0835e364dcb17091a245b0efdd26313
F4R3Juan58 <juan58coc@gmail.com>
2025-03-24 20:44:48 +0100

    Mejora UX/UI Login y Registro

--- TFGClient/obj/Debug/net8.0-ios/iossimulator-x64/TFGClient.AssemblyInfo.cs
+++ TFGClient/obj/Debug/net8.0-ios/iossimulator-x64/TFGClient.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("TFGClient")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0+80c8251558d00e22ab08a74587a85911cda99aa2")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0+7f7f24a9a0a9185662639f8f28866280c58d31ae")]
 [assembly: System.Reflection.AssemblyProductAttribute("TFGClient")]
 [assembly: System.Reflection.AssemblyTitleAttribute("TFGClient")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
