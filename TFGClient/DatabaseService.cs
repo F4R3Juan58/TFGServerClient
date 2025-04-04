1a99a8636a94c7a9dc1550c76edb701a89f3877a
F4R3Juan58 <juan58coc@gmail.com>
2025-04-04 13:17:51 +0200

    CA

--- TFGClient/DatabaseService.cs
+++ TFGClient/DatabaseService.cs
@@ -28,7 +28,7 @@ namespace TFGClient
 
     public class DatabaseService
     {
-        private readonly string connectionString = "server=13.38.70.221;user=admin;database=DiscordDatabase;port=3306;password=User@123;";
+        private readonly string connectionString = "server=13.38.70.221;user=tfg;database=DiscordDatabase;port=3306;password=2DamTfg;";
 
         public ObservableCollection<Comunidad> ObtenerComunidades()
         {
