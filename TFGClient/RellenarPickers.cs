bb1b285363224fb9f611b272d02fcdbf265312e4
F4R3Juan58 <juan58coc@gmail.com>
2025-04-04 12:04:56 +0200

    1

--- TFGClient/RellenarPickers.cs
+++ TFGClient/RellenarPickers.cs
@@ -1,12 +1,40 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
+﻿using System.Collections.ObjectModel;
 using System.Text;
-using System.Threading.Tasks;
+using MySql.Data.MySqlClient;
 
 namespace TFGClient
 {
     class RellenarPickers
     {
+        private string connectionString = "server=13.38.70.221;user=admin;database=DiscordDatabase;port=3306;password=User@123;";
+
+        public ObservableCollection<Tareas> cargarComunidadAutonoma()
+        {
+            var tareas = new ObservableCollection<Tareas>();
+            var Query = "SELECT * FROM Tarea";
+            using (MySqlConnection connection = new MySqlConnection(connectionString))
+            {
+                connection.Open();
+
+                // Verifica si la tabla Departamento tiene datos
+                
+                using (var command = new MySqlConnection(Query, connection))
+                {
+                    using (var reader = command.ExecuteReader())
+                    {
+                        while (reader.Read())
+                        {
+                            tareas.Add(new Tareas
+                            {
+                                Id = reader.GetInt32(0),
+                                Marcado = reader.GetBoolean(1),
+                                Nombre = reader.GetString(2),
+                            });
+                        }
+                    }
+                }
+            }
+            return tareas;
+        }
     }
 }
