ec039f4abd4a4c0ad132925a9b3cff52cee95ee1
F4R3Juan58 <juan58coc@gmail.com>
2025-05-04 15:20:08 +0200

    Visuales

--- TFGServer/main.py
+++ TFGServer/main.py
@@ -1,23 +1,25 @@
-import discord
-from discord.ext import commands
+import disnake
+from disnake.ext import commands
 import config
 import os
 
-bot = commands.Bot(command_prefix="!!", intents=discord.Intents.all())
+bot = commands.Bot(command_prefix="!!", intents=disnake.Intents.all())
 
 ####    Inicio del Bot      ####
 
 @bot.event
-async def on_ready():  # Asegúrate de que 'on_ready' sea una corutina.
+async def on_ready():
     print(f'Bot conectado como {bot.user.name}')
-    await load_extensions()  # Llamar a load_extensions sin await
+    await load_extensions()
 
 async def load_extensions():
     print("Cargando extensiones")
     for filename in os.listdir("./cogs"):
         if filename.endswith(".py"):
-            # Cargar la extensión sin usar await
-            await bot.load_extension(f"cogs.{filename[:-3]}")
+            try:
+                await bot.load_extension(f"cogs.{filename[:-3]}")
+            except Exception as e:
+                print(f"No se pudo cargar {filename}: {e}")
     print("Extensiones cargadas.")
 
 #### Token ####
