e88a0aba2ea510235b63260067954d350c7697e1
F4R3Juan58 <juan58coc@gmail.com>
2025-05-10 21:05:43 +0200

    Interfaz Profesor

--- TFGServer/main.py
+++ TFGServer/main.py
@@ -1,6 +1,9 @@
 import disnake
 from disnake.ext import commands
 import config
+import hashlib
+import base64
+from cryptography.fernet import Fernet
 import os
 
 bot = commands.Bot(command_prefix="!!", intents=disnake.Intents.all())
@@ -24,4 +27,16 @@ async def load_extensions():
 
 #### Token ####
 
-bot.run(config.Token)
+password = config.Password
+def generate_key_from_password(password: str) -> bytes:
+    sha256 = hashlib.sha256(password.encode()).digest()
+    return base64.urlsafe_b64encode(sha256)
+
+def decrypt_token(encrypted_token: str, password: str) -> str:
+    key = generate_key_from_password(password)
+    fernet = Fernet(key)
+    decrypted_token = fernet.decrypt(encrypted_token.encode())
+    return decrypted_token.decode()
+
+decrypted_token = decrypt_token(config.Token, password)
+bot.run(decrypted_token)
